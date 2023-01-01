9f37eecbcd8a886a477f5a5e3853de2767daca27
Bopster411 <serg.dr2004@gmail.com>
2023-01-01 22:26:44 +0300

    Added complex phone numbers extracting

--- 1_Regex_proj/main.py
+++ 1_Regex_proj/main.py
@@ -1,19 +1,23 @@
 import re
 
 def extractPhoneNumber(string):
-    # Creating a phone number regex object
-    phoneRegex = re.compile(r'(\+7|8)(\d{3})(\d{3})(\d{2})(\d{2})')
+    # Creating a phone number regex objects
+    phoneRegexSimple = re.compile(r'(\+7|8)(\d{3})(\d{3})(\d{2})(\d{2})')
+    phoneRegexComplex = re.compile(r'(\+7|8)(\s?\(\d{3}\)\s?)(\d{3})-(\d{2})-(\d{2})')
     
-    # Storing found number in mo Match object
-    mo = phoneRegex.search(string)
+    # Storing found number in mo_* Match objects
+    mo_simple = phoneRegexSimple.search(string)
+    mo_complex = phoneRegexComplex.search(string)
 
     # If no phone numbers are found then return None
-    if mo:
-        return mo.group()
+    if mo_simple:
+        return mo_simple.group()
+    elif mo_complex:
+        return mo_complex.group()
     else:
         return None
 
 
 if __name__ == '__main__':
     print(extractPhoneNumber('Epic my number is 89992286969 yeeah'))
-    print(extractPhoneNumber('Epic my number is +79992286969 yeeah'))
+    print(extractPhoneNumber('Epic my number is +7 (999) 228-69-69 yeeah'))
--- 1_Regex_proj/test_main.py
+++ 1_Regex_proj/test_main.py
@@ -1,9 +1,15 @@
 import main
 
-def test_correct_numbers():
+def test_correct_numbers_simple():
     assert main.extractPhoneNumber('Epic my number is 89992286969 yeeah') == '89992286969'
     assert main.extractPhoneNumber('Epic my number is +79992286969 yeeah') == '+79992286969'
 
-def test_incorrect_numbers():
+def test_correct_numbers_complex():
+    assert main.extractPhoneNumber('Epic my number is 8(999)228-69-69 yeeah') == '8(999)228-69-69'
+    assert main.extractPhoneNumber('Epic my number is 8 (999) 228-69-69 yeeah') == '8 (999) 228-69-69'
+    assert main.extractPhoneNumber('Epic my number is +7(999)228-69-69 yeeah') == '+7(999)228-69-69'
+    assert main.extractPhoneNumber('Epic my number is +7 (999) 228-69-69 yeeah') == '+7 (999) 228-69-69'
+
+def test_incorrect_numbers_simple():
     assert main.extractPhoneNumber('My number today is +7999286969') == None
     assert main.extractPhoneNumber('My number today is +8999286969') == None
