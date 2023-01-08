ccc3ed9f7db43550102383cf8aa886cca2c38807
Bopster411 <serg.dr2004@gmail.com>
2023-01-08 21:11:49 +0300

    Changed functions' names

--- 1_Regex_proj/phnumbers.py
+++ 1_Regex_proj/phnumbers.py
@@ -1,15 +1,15 @@
 import re
 
-def extractPhoneNumber(string, all=False):
+def extract_phone_number(string, all=False):
     # Creating a phone number regex objects
-    phoneRegexSimple = re.compile(r'(\+7|8)(\d{3})(\d{3})(\d{2})(\d{2})')
-    phoneRegexComplex = re.compile(r'(\+7|8)\s?(\(\d{3}\))\s?(\d{3})-(\d{2})-(\d{2})')
+    phone_regex_simple = re.compile(r'(\+7|8)(\d{3})(\d{3})(\d{2})(\d{2})')
+    phone_regex_complex = re.compile(r'(\+7|8)\s?(\(\d{3}\))\s?(\d{3})-(\d{2})-(\d{2})')
     
     # If user wants to find all phone numbers in the string
     if all:
         # Storing found number in tuples
-        simple_phnumbers = phoneRegexSimple.findall(string)
-        complex_phnumbers = phoneRegexComplex.findall(string)
+        simple_phnumbers = phone_regex_simple.findall(string)
+        complex_phnumbers = phone_regex_complex.findall(string)
 
         # If no phone numbers are found then return None
         if simple_phnumbers or complex_phnumbers:
@@ -19,8 +19,8 @@ def extractPhoneNumber(string, all=False):
 
     else:
         # Storing found number in mo_* Match objects
-        mo_simple = phoneRegexSimple.search(string)
-        mo_complex = phoneRegexComplex.search(string)
+        mo_simple = phone_regex_simple.search(string)
+        mo_complex = phone_regex_complex.search(string)
 
         # If no phone numbers are found then return None
         if mo_simple:
@@ -31,5 +31,5 @@ def extractPhoneNumber(string, all=False):
             return None
 
 if __name__ == '__main__':
-    print(extractPhoneNumber('Epic my number is 89992286969 yeeah'))
-    print(extractPhoneNumber('Epic my number is +7 (999) 228-69-69 yeeah'))
+    print(extract_phone_number('Epic my number is 89992286969 yeeah'))
+    print(extract_phone_number('Epic my number is +7 (999) 228-69-69 yeeah'))
