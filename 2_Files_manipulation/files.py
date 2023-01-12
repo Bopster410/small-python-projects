7c90418a6120df24265c5f078dc75cf70e9cdfba
Bopster411 <serg.dr2004@gmail.com>
2023-01-12 22:10:53 +0300

    Added copy function

--- 2_Files_manipulation/files.py
+++ 2_Files_manipulation/files.py
@@ -1,9 +1,18 @@
-import logging
+import logging, shutil
+from pathlib import Path
 
 class fileManipulator():
     def __init__(self):
-        logging.debug('fileManipulator object has been created')
+        logging.info('fileManipulator object has been created')
+    
+    # Copies scr to dest (relative)
+    def copy(self, src, dest):
+        logging.info(f'copying {src} to {dest}')
+        p = Path.cwd()
+        shutil.copy(p / src, p / dest)
+
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='files.log', filemode='w', level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(funcName)s, %(lineno)d: %(message)s')
+    logging.basicConfig(filename='files.log', filemode='w', level=logging.INFO, format='%(asctime)s | %(levelname)s | %(funcName)s, %(lineno)d: %(message)s')
     fm = fileManipulator()
+    fm.copy('files.log', 'logs/files.log')
