8008813db291b6f701646395d2ba4d72833427d3
Bopster411 <serg.dr2004@gmail.com>
2023-01-14 22:45:03 +0300

    Okay now i'm sure it's fixed

--- 2_Files_manipulation/files.py
+++ 2_Files_manipulation/files.py
@@ -40,31 +40,33 @@ class fileManipulator():
             logging.info(f"created '{dir_to_create}' folders")
 
     # Creates zip-archive named [name] with [dir] in it
+    # TODO add input errors handlers
     def zip(self, dir, name):
         with zipfile.ZipFile(name, 'w') as dir_zipped:
             p_dir = Path(dir)
             # If [dir] is directory
             if p_dir.is_dir():
                 # Recursively add all files from the directory to the archive
-                for file in p_dir.glob('*'):
+                for file in p_dir.rglob('*'):
                     dir_zipped.write(file, compress_type=zipfile.ZIP_DEFLATED)
             # If [dir] is a single file actually
             else:
                 dir_zipped.write(dir, compress_type=zipfile.ZIP_DEFLATED)
-
     
+    # TODO add input errors handlers
     def unzip(self, dir, new_name=''):
-        dir_zipped = zipfile.ZipFile(dir)
-        dir_zipped.extractall(new_name) if len(new_name) else dir_zipped.extractall()
-        dir_zipped.close()
+        with zipfile.ZipFile(dir) as dir_zipped:
+            dir_zipped.extractall(new_name) if len(new_name) else dir_zipped.extractall()
     
 
 if __name__ == '__main__':
     logging.basicConfig(filename='files.log', filemode='w', level=logging.INFO, format='%(asctime)s | %(levelname)s | %(funcName)s, %(lineno)d: %(message)s')
     fm = fileManipulator()
-    fm.copy('files.log', 'logss/files.log')
-    fm.move('logss/files.log', 'logs/files.log')
-    fm.zip('files.log', 'loggs.zip')
-    fm.zip('logs', 'logs.zip')
-    fm.unzip('logs.zip', 'l o g s')
-    fm.unzip('logs.zip')
+    # fm.copy('files.log', 'logss/files.log')
+    # fm.move('logss/files.log', 'logs/files.log')
+    # fm.zip('files.log', 'loggs.zip')
+    # fm.zip('logs', 'logs.zip')
+    # fm.unzip('logs.zip', 'l o g s')
+    # fm.unzip('logs.zip')
+    fm.zip('test', 'test.zip')
+    fm.unzip('test.zip', 'test_unzipped')
