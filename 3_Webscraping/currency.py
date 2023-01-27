e34a6696510107b8b9b7170650955e6258173a34
Bopster411 <serg.dr2004@gmail.com>
2023-01-27 13:10:51 +1000

    Added get method to Currency class

--- 3_Webscraping/currency.py
+++ 3_Webscraping/currency.py
@@ -2,3 +2,6 @@ class Currency():
     def __init__(self, value, currency):
         self.value = value
         self.currency = currency
+    
+    def get_info(self):
+        return f'{self.value} {self.currency}'
