a57a8c7c5f9e14ada0a6e24ff1854ef48a485aa6
Bopster411 <serg.dr2004@gmail.com>
2023-02-15 20:13:13 +0300

    Added Ruble class

--- 3_Webscraping/currency.py
+++ 3_Webscraping/currency.py
@@ -1,9 +1,19 @@
 class Dollar():
     def __init__(self, amount):
-        self.amount = amount
+        self.__amount = amount
     
     def __eq__(self, __o: object) -> bool:
-        return __o.amount == self.amount
+        return __o.__amount == self.__amount
 
     def times(self, multiplier) -> object:
-        return Dollar(self.amount * multiplier)
+        return Dollar(self.__amount * multiplier)
+
+class Ruble():
+    def __init__(self, amount):
+        self.__amount = amount
+    
+    def __eq__(self, __o: object) -> bool:
+        return __o.__amount == self.__amount
+
+    def times(self, multiplier) -> object:
+        return Dollar(self.__amount * multiplier)
--- 3_Webscraping/test_currency.py
+++ 3_Webscraping/test_currency.py
@@ -1,12 +1,16 @@
-import currency
+from currency import Dollar, Ruble
+
 
 def test_multiplication():
-    five = currency.Dollar(5)
-    product = five.times(3)
-    assert product.amount == 15
-    product = five.times(4)
-    assert product.amount== 20
+    five = Dollar(5)
+    assert Dollar(15) == five.times(3)
+    assert Dollar(20) == five.times(4)
 
 def test_equals():
-    assert currency.Dollar(5) == currency.Dollar(5)
-    assert currency.Dollar(5) != currency.Dollar(6)
+    assert Ruble(5) == Ruble(5)
+    assert Ruble(5) != Ruble(6)
+
+def test_multiplication():
+    five = Dollar(5)
+    assert Dollar(15) == five.times(3)
+    assert Dollar(20) == five.times(4)
