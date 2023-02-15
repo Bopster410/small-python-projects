922e75f4d8a7e625fd083bf336fa10680cc80155
Bopster411 <serg.dr2004@gmail.com>
2023-02-15 15:16:08 +0300

    Removed Dollar's side effects

--- 3_Webscraping/test_currency.py
+++ 3_Webscraping/test_currency.py
@@ -2,5 +2,7 @@ import currency
 
 def test_multiplication():
     five = currency.Dollar(5)
-    five.times(3)
-    assert five.amount == 15
+    product = five.times(3)
+    assert product.amount == 15
+    product = five.times(4)
+    assert product.amount== 20
