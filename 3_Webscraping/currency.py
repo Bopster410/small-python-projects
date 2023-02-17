8413732f1ed7fd3f1b2e1a905311411023852d8f
Bopster411 <serg.dr2004@gmail.com>
2023-02-17 23:24:01 +0300

    Added factories for Dollar and Ruble objects

--- 3_Webscraping/currency.py
+++ 3_Webscraping/currency.py
@@ -1,14 +1,20 @@
 class Money():
-    def __init__(self, amount):
+    def __init__(self, amount=0):
         self._amount = amount
 
     def __eq__(self, __o: object) -> bool:
         return __o._amount == self._amount and type(self) == type(__o)
+    
+    def dollar(self):
+        return Dollar(self._amount)
+    
+    def ruble(self):
+        return Ruble(self._amount)
 
 class Dollar(Money):
-    def times(self, multiplier) -> object:
+    def times(self, multiplier) -> Money:
         return Dollar(self._amount * multiplier)
 
 class Ruble(Money):
-    def times(self, multiplier) -> object:
+    def times(self, multiplier) -> Money:
         return Ruble(self._amount * multiplier)
--- 3_Webscraping/test_currency.py
+++ 3_Webscraping/test_currency.py
@@ -1,13 +1,13 @@
-from currency import Dollar, Ruble
+from currency import Dollar, Ruble, Money
 
 def test_equals_dollar():
-    assert Dollar(5) == Dollar(5)
-    assert Dollar(5) != Dollar(6)
-    assert Ruble(5) == Ruble(5)
-    assert Ruble(5) != Ruble(6)
-    assert Ruble(5) != Dollar(5)
+    assert Money(5).dollar() == Money(5).dollar()
+    assert Money(5).dollar() != Money(6).dollar()
+    assert Money(5).ruble() == Money(5).ruble()
+    assert Money(5).ruble() != Money(6).ruble()
+    assert Money(5).ruble() != Money(5).dollar()
 
 def test_multiplication():
-    five = Dollar(5)
-    assert Dollar(15) == five.times(3)
-    assert Dollar(20) == five.times(4)
+    five = Money(5).dollar()
+    assert Money(15).dollar() == five.times(3)
+    assert Money(20).dollar() == five.times(4)
