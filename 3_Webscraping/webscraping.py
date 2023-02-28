64f6a03fc44c79e08af71486991799f5ec1d2ec6
Bopster411 <serg.dr2004@gmail.com>
2023-02-28 21:17:13 +0300

    Added find method and changed get_html to work with Internet

--- 3_Webscraping/webscraping.py
+++ 3_Webscraping/webscraping.py
@@ -1,13 +1,22 @@
-import logging, webbrowser, shelve, bs4
+import logging, webbrowser, shelve, bs4, requests
 
 class WebScaper():
     def __init__(self):
         logging.info("WebScraper object has been created")
     
-    def get_html(self, name):
-        file = open(name)
-        text = bs4.BeautifulSoup(file, 'html.parser')
+    def get_html(self, name, local=False):
+        if local:
+            html = open(name)
+            text = bs4.BeautifulSoup(html, 'html.parser')
+        else:
+            html = requests.get(name)
+            text = bs4.BeautifulSoup(html.text, 'html.parser')
         return text
+    
+    def find(self, src, selector):
+        html = self.get_html(src)
+        result = html.select(selector)
+        return result
 
 class WebPages():
     def __init__(self):
@@ -38,4 +47,4 @@ class WebPages():
 if __name__ == '__main__':
     logging.basicConfig(filename='files.log', filemode='w', level=logging.INFO, format='%(asctime)s | %(levelname)s | %(funcName)s, %(lineno)d: %(message)s')
     ws = WebScaper()
-    print(ws.get_html('F:\\infsem\\1 sem\\4\\main.html'))
+    print(ws.get_html('https://www.banki.ru/products/currency/rub/'))
