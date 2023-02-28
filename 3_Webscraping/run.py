b681ee30c264e9369024b84412d6dc2c1563e8c3
Bopster411 <serg.dr2004@gmail.com>
2023-02-28 21:53:16 +0300

    Changed entrypoint

--- 3_Webscraping/run.py
+++ 3_Webscraping/run.py
@@ -1,10 +1,5 @@
-import webscraping as ws
+from gui import App
 
 if __name__ == '__main__':
-    ws = ws.WebScaper()
-    selector = 'body > div.layout-wrapper.padding-top-default.bg-white.position-relative \
-> div.layout-columns-wrapper > main > section:nth-child(5) > div.currency-board__container \
-> div.currency-board > div.currency-board__table > div:nth-child(2) > div > div \
-> div.currency-board__field > div.currency-board__slot__value > span.currency-board__value.display-inline-block'
-    rubles = float(ws.find('https://www.banki.ru/products/currency/rub/', selector)[0].get_text().replace(',', '.'))
-    print(f'1 USD == {rubles} RUB')
+    app = App()
+    app.mainloop()
