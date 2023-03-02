b9eabf0b69834512b9db7863d960f157aaac0c08
Bopster411 <serg.dr2004@gmail.com>
2023-03-02 08:49:21 +0300

    Debug

--- 3_Webscraping/gui.py
+++ 3_Webscraping/gui.py
@@ -12,11 +12,11 @@ class MoneyApp(ctk.CTk):
         self.label.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
     
     def __get_new_currency(self):
-        self.selector = 'body > div.layout-wrapper.padding-top-default.bg-white.position-relative \
+        selector = 'body > div.layout-wrapper.padding-top-default.bg-white.position-relative \
 > div.layout-columns-wrapper > main > section:nth-child(5) > div.currency-board__container \
 > div.currency-board > div.currency-board__table > div:nth-child(2) > div > div \
 > div.currency-board__field > div.currency-board__slot__value > span.currency-board__value.display-inline-block'
-        # rubles = float(self.ws.find('https://www.banki.ru/products/currency/rub/', self.selector)[0].get_text().replace(',', '.'))
+        # rubles = float(self.ws.find('https://www.banki.ru/products/currency/rub/', selector)[0].get_text().replace(',', '.'))
         rubles = 75.5
         return rubles
 
