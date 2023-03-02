bffa9a475ba2d99ccf217b9043c88b88cd05cc02
Bopster411 <serg.dr2004@gmail.com>
2023-03-02 09:12:03 +0300

    Added text_box

--- 3_Webscraping/gui.py
+++ 3_Webscraping/gui.py
@@ -9,7 +9,11 @@ class MoneyApp(ctk.CTk):
         self.geometry('500x500')
 
         self.label = ctk.CTkLabel(self, text=f'1 USD == {self.__get_new_currency()} RUB', font=('Arial', 25))
-        self.label.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+        self.label.place(relx=0.5, rely=0.1, anchor=ctk.CENTER)
+
+        self.text_box = ctk.CTkTextbox(self, width=100, height=50, corner_radius=8, font=('Arial', 20))
+        self.text_box.place(relx=0.2, rely=0.5, anchor=ctk.CENTER)
+        self.text_box.insert('0.0', 'USD')
     
     def __get_new_currency(self):
         selector = 'body > div.layout-wrapper.padding-top-default.bg-white.position-relative \
@@ -19,7 +23,6 @@ class MoneyApp(ctk.CTk):
         # rubles = float(self.ws.find('https://www.banki.ru/products/currency/rub/', selector)[0].get_text().replace(',', '.'))
         rubles = 75.5
         return rubles
-
 
 
 if __name__ == '__main__':
