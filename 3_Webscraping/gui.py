68a0612c6a2713599c1df85b52dfa43053688401
Bopster411 <serg.dr2004@gmail.com>
2023-03-03 18:59:52 +0300

    Moved all converter frame code to separate class

--- 3_Webscraping/gui.py
+++ 3_Webscraping/gui.py
@@ -1,5 +1,31 @@
 import customtkinter as ctk, webscraping, logging  
 
+
+class ConverterFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        # Input value
+        self.input_lbl = ctk.CTkEntry(self, width=140, height=50, corner_radius=8, font=('Arial', 20))
+        self.input_lbl.place(relx=0.2, rely=0.5)
+
+        # Button to show the result
+        self.enter_btn = ctk.CTkButton(self, height=50, text='Enter', command=self.enter_event, font=('Arial', 20))
+        self.enter_btn.place(relx=0.6, rely=0.5)
+
+        # Result label
+        self.result_lbl = ctk.CTkLabel(self, width=200, height=50, corner_radius=8, font=('Arial', 20), text='')
+        self.result_lbl.place(relx=0.4, rely=0.7)
+
+    # Callback for the enter button (enter_btn)
+    # TODO use cache to get exchange rate
+    # TODO regex to validate input
+    def enter_event(self):
+        input = float(self.input_lbl.get())
+        logging.debug(f'entered value: {input}')
+        self.result_lbl.configure(text=f'{round(input * get_new_exchange_rate(), 2)} RUB')
+
+
 class MoneyApp(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -12,43 +38,24 @@ class MoneyApp(ctk.CTk):
         self.geometry('500x500')
 
         # Label showing current exchange rate
-        self.exchange_rate_lbl = ctk.CTkLabel(self, text=f'1 USD == {self.__get_new_exchange_rate()} RUB', font=('Arial', 25))
+        self.exchange_rate_lbl = ctk.CTkLabel(self, text=f'1 USD == {get_new_exchange_rate()} RUB', font=('Arial', 25))
         self.exchange_rate_lbl.pack(pady=30)
 
         # Frame with converter
-        self.converter_frame = ctk.CTkFrame(self, width=450, height=350)
+        self.converter_frame = ConverterFrame(self, width=450, height=400)
         self.converter_frame.pack()
 
-        # Input value
-        self.input_lbl = ctk.CTkEntry(self.converter_frame, width=140, height=50, corner_radius=8, font=('Arial', 20))
-        self.input_lbl.place(relx=0.2, rely=0.5)
 
-        # Button to show the result
-        self.enter_btn = ctk.CTkButton(self.converter_frame, height=50, text='Enter', command=self.enter_event, font=('Arial', 20))
-        self.enter_btn.place(relx=0.6, rely=0.5)
-
-        # Result label
-        self.result_lbl = ctk.CTkLabel(self.converter_frame, width=200, height=50, corner_radius=8, font=('Arial', 20), text='')
-        self.result_lbl.place(relx=0.4, rely=0.7)
-    
-    # Callback for the enter button (enter_btn)
-    # TODO use cache to get exchange rate
-    # TODO regex to validate input
-    def enter_event(self):
-        input = float(self.input_lbl.get())
-        logging.debug(f'entered value: {input}')
-        self.result_lbl.configure(text=f'{round(input * self.__get_new_exchange_rate(), 2)} RUB')
-    
-    # Returns current usd to rub exchange rate
-    # TODO use cache to store exchange rate
-    def __get_new_exchange_rate(self):
-        selector = 'body > div.layout-wrapper.padding-top-default.bg-white.position-relative \
+# Returns current usd to rub exchange rate
+# TODO use cache to store exchange rate
+def get_new_exchange_rate():
+    selector = 'body > div.layout-wrapper.padding-top-default.bg-white.position-relative \
 > div.layout-columns-wrapper > main > section:nth-child(5) > div.currency-board__container \
 > div.currency-board > div.currency-board__table > div:nth-child(2) > div > div \
 > div.currency-board__field > div.currency-board__slot__value > span.currency-board__value.display-inline-block'
-        # rubles = float(self.ws.find('https://www.banki.ru/products/currency/rub/', selector)[0].get_text().replace(',', '.'))
-        rubles = 75.5
-        return rubles
+    # rubles = float(self.ws.find('https://www.banki.ru/products/currency/rub/', selector)[0].get_text().replace(',', '.'))
+    rubles = 75.5
+    return rubles
 
 
 if __name__ == '__main__':
