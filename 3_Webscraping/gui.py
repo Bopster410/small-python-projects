2b17d4ca23a78512d085ae4b5be8bded3169b485
Bopster411 <serg.dr2004@gmail.com>
2023-03-13 12:41:25 +0300

    Changed layout

--- 3_Webscraping/gui.py
+++ 3_Webscraping/gui.py
@@ -8,25 +8,26 @@ class ConverterFrame(ctk.CTkFrame):
         # Configure grid
         self.grid_columnconfigure((0, 1), weight=1)
         self.grid_columnconfigure(2, weight=2)
-
-        # Input value
-        self.input_lbl = ctk.CTkEntry(self, corner_radius=8, font=('Arial', 20))
-        self.input_lbl.grid(row=0, column=0, padx=20, pady=10, sticky='ewsn')
-
-        # Input currency
+# Input currency
         self.currency_menu_input = ctk.CTkOptionMenu(self, values=['USD', 'RUB'], command=self.currency_menu_event)
-        self.currency_menu_input.grid(row=0, column=1, sticky='w')
-        # Output currency
+        self.currency_menu_input.grid(row=0, column=0, padx=20, pady=10, sticky='w')
+# Output currency
         self.currency_menu_output = ctk.CTkOptionMenu(self, values=['RUB', 'USD' ], command=self.currency_menu_event)
-        self.currency_menu_output.grid(row=0, column=2, sticky='w')
-        
+        self.currency_menu_output.grid(row=0, column=1, pady=20,  sticky='w')
         # Button to show the result
         self.enter_btn = ctk.CTkButton(self, height=50, text='Enter', command=self.enter_event, font=('Arial', 20))
-        self.enter_btn.grid(row=0, column=3, padx=20, pady=10, sticky='esn')
+        self.enter_btn.grid(row=0, column=2, padx=20, pady=10, rowspan=2, sticky='esn')
+        # Input value
+        self.input_entry = ctk.CTkEntry(self, height=50, corner_radius=8, font=('Arial', 20))
+        self.input_entry.grid(row=1, column=0, padx=20, pady=10 , sticky='ewsn')
+
+        
+        
+        
 
         # Result label
-        self.result_lbl = ctk.CTkLabel(self, width=200, height=50, font=('Arial', 20), text='')
-        self.result_lbl.grid(row=1, column=0, columnspan=3, pady=20, sticky='ew')
+        self.result_lbl = ctk.CTkLabel(self,  font=('Arial', 20), text='')
+        self.result_lbl.grid(row=1, column=1, sticky='ew')
         
         # Bank for currency conversion
         self.bank = currency.Bank()
@@ -38,7 +39,7 @@ class ConverterFrame(ctk.CTkFrame):
     # TODO use cache to get exchange rate
     # TODO regex to validate input
     def enter_event(self):
-        input = currency.Money(self.currency_menu_input.get(), int(self.input_lbl.get()))
+        input = currency.Money(self.currency_menu_input.get(), int(self.input_entry.get()))
         logging.debug(f'entered value: {input.amount()}')
         output = input.reduce(self.currency_menu_output.get(), bank=self.bank)
         self.result_lbl.configure(text=f'{output}')
