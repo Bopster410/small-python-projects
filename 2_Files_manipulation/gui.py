87b6252ff4b546a5c226694e0a8e5b126709b66c
Bopster411 <serg.dr2004@gmail.com>
2023-04-08 15:35:13 +0300

    Added directory changing

--- 2_Files_manipulation/gui.py
+++ 2_Files_manipulation/gui.py
@@ -1,20 +1,35 @@
-import customtkinter as ctk
+import customtkinter as ctk, logging
 from pathlib import Path
 
 class FilesApp(ctk.CTk):
     def __init__(self):
+        logging.info('FilesApp object was created')
         super().__init__()
 
         self.title('Files app')
         self.geometry('700x500')
 
-        cwd = Path.cwd()
-        cwd_str = ctk.StringVar(value=str(cwd))
-        inner_dirs = [child.name for child in cwd.iterdir()]
-        inner_dirs_lbls = [ctk.CTkLabel(self, text=str(child), font=('Arial', 17)) for child in inner_dirs]
+        self.cwd = Path.cwd()
+        self.cwd_str = ctk.StringVar(value=str(self.cwd))
+ 
+        self.current_dir = ctk.CTkLabel(self, textvariable=self.cwd_str, font=('Arial', 20))
+        self.current_dir.grid(column=0, row=0, pady=10, sticky='ew')
+        
+        inner_dirs = [child.name for child in self.cwd.iterdir()]
+        self.inner_dirs_btns = [ctk.CTkButton(self, text=str(child), font=('Arial', 17), command=self.change_dir_event(child)) for child in inner_dirs]
 
-        for i in range(len(inner_dirs_lbls)):
-            inner_dirs_lbls[i].grid(column=0, row=i+1, sticky='w', padx=10)
+        for i in range(len(self.inner_dirs_btns)):
+            self.inner_dirs_btns[i].grid(column=0, row=i+1, sticky='w', padx=10)
 
-        self.current_dir = ctk.CTkLabel(self, textvariable=cwd_str, font=('Arial', 20))
-        self.current_dir.grid(column=0, row=0, sticky='ew')
+    def change_dir_event(self, new_dir):
+        def change_dir():
+            self.cwd = self.cwd / new_dir
+            self.cwd_str.set(str(self.cwd))
+            inner_dirs = [child.name for child in self.cwd.iterdir()]
+            for i in range(len(self.inner_dirs_btns)):
+                self.inner_dirs_btns[i].grid_forget()
+            self.inner_dirs_btns = [ctk.CTkButton(self, text=str(child), font=('Arial', 17), command=self.change_dir_event(child)) for child in inner_dirs]
+            for i in range(len(self.inner_dirs_btns)):
+                self.inner_dirs_btns[i].grid(column=0, row=i+1, sticky='w', padx=10)
+            
+        return change_dir
