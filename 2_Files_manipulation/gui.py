6d749fa982931eb7d39088131cc9df106ed8d083
Bopster411 <serg.dr2004@gmail.com>
2023-04-08 15:01:31 +0300

    Added label with current working directory

--- 2_Files_manipulation/gui.py
+++ 2_Files_manipulation/gui.py
@@ -3,10 +3,13 @@ from pathlib import Path
 
 class FilesApp(ctk.CTk):
     def __init__(self):
-        self.super()
+        super().__init__()
 
         self.title('Files app')
         self.geometry('700x500')
 
-        self.current_dir = ctk.label(self, text='[Current directory]', font=('Arial', 25))
-        self.current_dir.grid(column=0, row=0, ancher=ctk.CENTER)
+        cwd = ctk.StringVar(value=str(Path.cwd()))
+        
+
+        self.current_dir = ctk.CTkLabel(self, textvariable=cwd, font=('Arial', 25))
+        self.current_dir.grid(column=0, row=0, sticky='ew')
