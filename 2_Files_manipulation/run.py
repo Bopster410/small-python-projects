3172b0c90bfd5500cfd9bc78f9948edd5eafb628
Bopster411 <serg.dr2004@gmail.com>
2023-04-08 15:23:41 +0300

    Added logging

--- 2_Files_manipulation/run.py
+++ 2_Files_manipulation/run.py
@@ -1,6 +1,8 @@
 from gui import FilesApp
+import logging
 
 def main():
+    logging.basicConfig(filename='files.log', filemode='w', level=logging.INFO, format='%(asctime)s | %(levelname)s | %(funcName)s, %(lineno)d: %(message)s')
     app = FilesApp()
     app.mainloop()
 
