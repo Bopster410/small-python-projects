05780c92c5a09d1755a0069604289881259da1d1
Bopster411 <serg.dr2004@gmail.com>
2023-04-08 15:09:33 +0300

    Added child directories labels

--- 2_Files_manipulation/gui.py
+++ 2_Files_manipulation/gui.py
@@ -8,8 +8,13 @@ class FilesApp(ctk.CTk):
         self.title('Files app')
         self.geometry('700x500')
 
-        cwd = ctk.StringVar(value=str(Path.cwd()))
-        
+        cwd = Path.cwd()
+        cwd_str = ctk.StringVar(value=str(cwd))
+        inner_dirs = [child.name for child in cwd.iterdir()]
+        inner_dirs_lbls = [ctk.CTkLabel(self, text=str(child), font=('Arial', 17)) for child in inner_dirs]
 
-        self.current_dir = ctk.CTkLabel(self, textvariable=cwd, font=('Arial', 25))
+        for i in range(len(inner_dirs_lbls)):
+            inner_dirs_lbls[i].grid(column=0, row=i+1, sticky='w', padx=10)
+
+        self.current_dir = ctk.CTkLabel(self, textvariable=cwd_str, font=('Arial', 20))
         self.current_dir.grid(column=0, row=0, sticky='ew')
