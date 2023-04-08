3fa690a934b613f3331adf1878370da30116549e
Bopster411 <serg.dr2004@gmail.com>
2023-04-08 15:53:49 +0300

    Fixed problem with files

--- 2_Files_manipulation/gui.py
+++ 2_Files_manipulation/gui.py
@@ -23,13 +23,14 @@ class FilesApp(ctk.CTk):
 
     def change_dir_event(self, new_dir):
         def change_dir():
-            self.cwd = self.cwd / new_dir
-            self.cwd_str.set(str(self.cwd))
-            inner_dirs = [child.name for child in self.cwd.iterdir()]
-            for i in range(len(self.inner_dirs_btns)):
-                self.inner_dirs_btns[i].grid_forget()
-            self.inner_dirs_btns = [ctk.CTkButton(self, text=str(child), font=('Arial', 17), command=self.change_dir_event(child)) for child in inner_dirs]
-            for i in range(len(self.inner_dirs_btns)):
-                self.inner_dirs_btns[i].grid(column=0, row=i+1, sticky='w', padx=10)
+            if (self.cwd / new_dir).is_dir():
+                self.cwd = self.cwd / new_dir
+                self.cwd_str.set(str(self.cwd))
+                inner_dirs = [child.name for child in self.cwd.iterdir()]
+                for i in range(len(self.inner_dirs_btns)):
+                    self.inner_dirs_btns[i].grid_forget()
+                self.inner_dirs_btns = [ctk.CTkButton(self, text=str(child), font=('Arial', 17), command=self.change_dir_event(child)) for child in inner_dirs]
+                for i in range(len(self.inner_dirs_btns)):
+                    self.inner_dirs_btns[i].grid(column=0, row=i+1, sticky='w', padx=10)
             
         return change_dir
