514f5a89c6903384ba3082f1a8e34bddcc5e472f
Bopster411 <serg.dr2004@gmail.com>
2023-04-14 09:54:04 +0300

    Customized a bit

--- 2_Files_manipulation/gui.py
+++ 2_Files_manipulation/gui.py
@@ -9,6 +9,7 @@ class FilesApp(ctk.CTk):
         # Init window settings
         self.title('Files app')
         self.geometry('700x500')
+        self.columnconfigure((0, 1), weight=1)
 
         # Current working directory
         self.cwd = Path.cwd()
@@ -16,9 +17,11 @@ class FilesApp(ctk.CTk):
  
         # Label for cwd
         self.current_dir = ctk.CTkLabel(self, textvariable=self.cwd_str, font=('Arial', 20))
-        self.current_dir.grid(column=0, row=0, pady=10, sticky='w')
+        self.current_dir.grid(column=0, row=0, pady=10, padx=10, sticky="w")
         
         # Child directories inside cwd
+        self.dirs_frame = ctk.CTkFrame(self, width=400, height=500, border_width=1, border_color="black")
+        self.dirs_frame.grid(column=0, row=1, columnspan=2, sticky="we")
         self.inner_dirs_btns = self.__form_inner_dirs()
         self.__grid_all(self.inner_dirs_btns, first_row=1)
 
@@ -41,7 +44,8 @@ class FilesApp(ctk.CTk):
         # Child directories inside cwd
         inner_dirs = (['..'] if self.cwd.parent != self.cwd else []) + [child.name for child in self.cwd.iterdir()]
         # ..and buttons for all of them
-        return [ctk.CTkButton(self, text=child, font=('Arial', 17), command=self.change_dir_event(child)) for child in inner_dirs]
+        return [ctk.CTkButton(self.dirs_frame, text=child, fg_color="white", border_color="#0F0F0F", border_width=2, text_color="black",
+                              font=('Arial', 17), anchor="w", width=400, command=self.change_dir_event(child)) for child in inner_dirs]
     
     def __grid_forget_all(self, objects):
         for i in range(len(objects)):
@@ -49,5 +53,5 @@ class FilesApp(ctk.CTk):
 
     def __grid_all(self, objects, first_row=0):
         for i in range(len(objects)):
-            objects[i].grid(column=0, row=i+first_row, sticky='w', padx=10)
+            objects[i].grid(column=0, row=i+first_row, columnspan=2, sticky='we', padx=10)
  
