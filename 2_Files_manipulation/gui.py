57245c5765ccf34e57842579eb1b2171f785e2fc
Bopster411 <serg.dr2004@gmail.com>
2023-04-23 22:24:59 +0300

    Added reload_dirs function and divided copy and paste commands to 2 separate menu items

--- 2_Files_manipulation/gui.py
+++ 2_Files_manipulation/gui.py
@@ -20,17 +20,28 @@ class FilesApp(ctk.CTk):
         self.current_dir.grid(column=0, row=0, pady=10, padx=10, sticky="w")
         
         # Popup menu
-        self.popup = tk.Menu(self, tearoff=False)
-        self.popup.add_command(label='copy', command=self.popup_copy_command())
-        self.popup.add_command(label='paste', command=self.popup_paste_command())
+        self.popup_file = tk.Menu(self, tearoff=False)
+        self.popup_file.add_command(label='copy', command=self.popup_copy_command())
+        
+        self.popup_general = tk.Menu(self, tearoff=False)
+        self.popup_general.add_command(label='paste', command=self.popup_paste_command())
+
         self.selected_dir = None
         self.src_path = None
 
         # Child directories inside cwd
         self.dirs_frame = ctk.CTkFrame(self, width=400, height=500, border_width=1, border_color="black")
         self.dirs_frame.grid(column=0, row=1, columnspan=2, sticky="we")
+        self.dirs_frame.bind('<Button-3>', self.popup_general_command())
         self.inner_dirs_btns = self.__form_inner_dirs()
         self.__grid_all(self.inner_dirs_btns, first_row=1)
+
+
+    def reload_dirs(self):
+        self.__grid_forget_all(self.inner_dirs_btns)
+        self.inner_dirs_btns = self.__form_inner_dirs()
+        self.__grid_all(self.inner_dirs_btns, first_row=1)
+    
 
     def popup_copy_command(self):
         def popup_copy_command():
@@ -46,14 +57,22 @@ class FilesApp(ctk.CTk):
             if self.src_path != None:
                 fm = files.FileManipulator()
                 fm.copy(self.src_path, self.cwd / self.src_path.name)
+                self.reload_dirs()
         return popup_paste_command
     
-    def popup_command(self, dir: ctk.CTkButton):
-        def popup_command(event):
-            self.popup.tk_popup(event.x_root, event.y_root)
+    def popup_file_command(self, dir: ctk.CTkButton):
+        def popup_file_command(event):
+            self.popup_file.tk_popup(event.x_root, event.y_root)
             self.selected_dir = dir
             logging.debug(f'Current selected dir is {dir.cget("text")}')
-        return popup_command
+        return popup_file_command
+    
+    def popup_general_command(self):
+        def popup_general_command(event):
+            self.popup_general.tk_popup(event.x_root, event.y_root)
+            logging.debug('General popup was activated')
+        return popup_general_command
+
 
     def change_dir_event(self, new_dir):
         # If new_dir is buttons folder, open it,
@@ -65,9 +84,7 @@ class FilesApp(ctk.CTk):
                 else:
                     self.cwd = self.cwd / new_dir
                 self.cwd_str.set(str(self.cwd))
-                self.__grid_forget_all(self.inner_dirs_btns)
-                self.inner_dirs_btns = self.__form_inner_dirs()
-                self.__grid_all(self.inner_dirs_btns, first_row=1)
+                self.reload_dirs()
         return change_dir
     
     def dir_hover_event(self, dir: ctk.CTkButton):
@@ -80,6 +97,7 @@ class FilesApp(ctk.CTk):
             dir.configure(border_color="#0F0F0F")
         return leave_event
     
+
     def __form_inner_dirs(self):
         # Child directories inside cwd
         inner_dirs = (['..'] if self.cwd.parent != self.cwd else []) + [child.name for child in self.cwd.iterdir()]
@@ -91,7 +109,8 @@ class FilesApp(ctk.CTk):
             btn.bind('<Double-Button-1>', self.change_dir_event(child))
             btn.bind('<Motion>', self.dir_hover_event(btn))
             btn.bind('<Leave>', self.dir_leave_event(btn))
-            btn.bind('<Button-3>', self.popup_command(btn))
+            if child != '..':
+                btn.bind('<Button-3>', self.popup_file_command(btn))
             buttons.append(btn)
         return buttons
     
