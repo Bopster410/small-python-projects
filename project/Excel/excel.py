3119e338b198721f4ad8a5a447addab463259ae8
Bopster411 <serg.dr2004@gmail.com>
2023-05-06 20:24:24 +0300

    Printing all the marks for students

--- project/Excel/excel.py
+++ project/Excel/excel.py
@@ -1,7 +1,12 @@
 import openpyxl as xl
 
-a = xl.load_workbook('../../test_data/IU4.xlsx')
+a = xl.load_workbook('test_data/IU4.xlsx')
 sheet = a['ИУ4-23Б']
 
-for i in range(3, sheet.max_row - 1):
-    print(sheet.cell(row=i, column=1).value, sheet.cell(row=i, column=2).value)
+for row in sheet.iter_rows(min_row=3, max_row=sheet.max_row - 2):
+    print(f'{row[0].value}: ', end='')
+    for cell in row[1:]:
+        if cell.value == None:
+            print('')
+            break
+        print(cell.value, end=' ')
