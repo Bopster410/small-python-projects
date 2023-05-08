005ccf5095fba05d2e95b7dbea21bf09beff0f37
Bopster411 <serg.dr2004@gmail.com>
2023-05-08 19:33:17 +0300

    CTk was added

--- project/Excel/gui.py
+++ project/Excel/gui.py
@@ -1,6 +1,6 @@
-import tkinter as tk, openpyxl as xl, logging, excel
+import customtkinter as ctk, openpyxl as xl, logging, excel
 
-class ExcelFileFrame(tk.Frame):
+class ExcelFileFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
@@ -10,22 +10,22 @@ class ExcelFileFrame(tk.Frame):
         for row in sheet.iter_rows(min_row=0, max_row=sheet.max_row):
             for cell in row:
                 if cell.value != None:
-                    current_cell = tk.Label(self, text=cell.value)
-                else:
-                    current_cell = tk.Label(self, text='')
+                    current_cell = ctk.CTkLabel(self, text=cell.value)
+                # else:
+                #     current_cell = ctk.Label(self, text='')
                 current_cell.grid(row=cell.row-1, column=cell.column-1)
 
-class ExcelApp(tk.Frame):
+class ExcelApp(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.file_id_input = tk.Entry(self)
+        self.file_id_input = ctk.CTkEntry(self)
         self.file_id_input.grid(row=0, column=0, sticky='we')
 
-        self.input_confirm_btn = tk.Button(self, command=self.input_confirm_command)
+        self.input_confirm_btn = ctk.CTkButton(self, command=self.input_confirm_command)
         self.input_confirm_btn.grid(row=0, column=1)
 
-        self.frame = tk.Frame(self)
+        self.frame = ctk.CTkFrame(self)
         self.frame.grid(row=1, column=0, columnspan=2)
         self.file_frame = ExcelFileFrame(self.frame)
         self.file_frame.grid(row=0, column=0)
@@ -38,10 +38,11 @@ class ExcelApp(tk.Frame):
 
 if __name__ == '__main__':
     logging.basicConfig(filename='excel_gui.log', filemode='w', level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(funcName)s, %(lineno)d: %(message)s')
-    a = tk.Tk()
-    a.geometry('600x300')
+    a = ctk.CTk()
+    a.resizable(False, False)
+    # a.geometry('600x300')
 
-    a_fr = tk.Frame(a)
+    a_fr = ctk.CTkFrame(a)
     a_fr.grid(row=1, column=0, columnspan=2)
 
     app = ExcelApp(a_fr)
