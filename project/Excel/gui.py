a8c9bb575dd2fc8b3bb1e33a378170e04627ebf0
Bopster411 <serg.dr2004@gmail.com>
2023-07-03 22:30:27 +0300

    Now the app fills the window

--- project/Excel/gui.py
+++ project/Excel/gui.py
@@ -1,9 +1,9 @@
-import customtkinter as ctk, openpyxl as xl, logging, excel
+import customtkinter as ctk, openpyxl as xl, logging, excel, tkinter as tk
 
 
 class ExcelFileFrame(ctk.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
         self.table = {}
 
     def delete_workbook(self):
@@ -49,33 +49,33 @@ class ExcelFileFrame(ctk.CTkFrame):
 
 
 class ExcelApp(ctk.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        
         self.columnconfigure(0, weight=5)
         self.columnconfigure((1, 2), weight=1)
         self.columnconfigure(3, weight=12)
         self.columnconfigure(4, weight=2)
 
-        self.file_id_input = ctk.CTkEntry(self, width=120, placeholder_text='File name')
-        self.file_id_input.grid(row=0, column=0, sticky='we')
+        self.rowconfigure(1, weight=1)
+
+        self.file_id_input = ctk.CTkEntry(self, placeholder_text='File name')
+        self.file_id_input.grid(row=0, column=0, pady=5,sticky='we')
 
         self.drive_local_option = ctk.CTkOptionMenu(self, width=80, values=['drive', 'local'])
-        self.drive_local_option.grid(row=0, column=1, padx=2, sticky='w')
+        self.drive_local_option.grid(row=0, column=1, padx=2, pady=5,sticky='w')
         
-        self.input_confirm_btn = ctk.CTkButton(self, width=50, text='Enter', command=self.input_confirm_command)
-        self.input_confirm_btn.grid(row=0, column=2, sticky='w')
+        self.input_confirm_btn = ctk.CTkButton(self, text='Enter', command=self.input_confirm_command)
+        self.input_confirm_btn.grid(row=0, column=2, pady=5,sticky='w')
 
         self.load_from_drive_btn = ctk.CTkButton(self, text='load from drive', command=self.load_from_drive_command)
-        self.load_from_drive_btn.grid(row=0, column=3, sticky='e')
+        self.load_from_drive_btn.grid(row=0, column=3, pady=5, sticky='e')
 
         self.clear_btn = ctk.CTkButton(self, text='clear', command=self.clear_command)
-        self.clear_btn.grid(row=0, column=4, sticky='e')
+        self.clear_btn.grid(row=0, column=4, pady=5, sticky='e')
 
-        self.frame = ctk.CTkFrame(self)
-        self.frame.grid(row=1, column=0, columnspan=5)
-        self.excel_file = ExcelFileFrame(self.frame)
-        self.excel_file.grid(row=0, column=0)
+        self.excel_file = ExcelFileFrame(self)
+        self.excel_file.grid(row=1, column=0, columnspan=5, sticky="nsew")
     
     def input_confirm_command(self):
         choice = self.drive_local_option.get()
@@ -104,12 +104,15 @@ class ExcelApp(ctk.CTkFrame):
 if __name__ == '__main__':
     logging.basicConfig(filename='excel_gui.log', filemode='w', level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(funcName)s, %(lineno)d: %(message)s')
     a = ctk.CTk()
+    a.geometry("1024x800")
+    a.rowconfigure(0, weight=1)
+    a.columnconfigure(0, weight=1)
     a.resizable(False, False)
 
     # a_fr = ctk.CTkFrame(a)
     # a_fr.grid(row=1, column=0, columnspan=2)
 
-    app = ExcelApp(a)
-    app.grid(row=0, column=0)
+    app = ExcelApp(master=a, fg_color="transparent")
+    app.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
 
     a.mainloop()
