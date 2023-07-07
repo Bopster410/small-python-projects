da169b65787e3b15d6af36f2705c28536aa5e97c
Bopster411 <serg.dr2004@gmail.com>
2023-07-07 13:12:08 +0300

    Added dialog for opening local files

--- project/Excel/gui.py
+++ project/Excel/gui.py
@@ -78,53 +78,60 @@ class ExcelApp(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         
-        self.columnconfigure(0, weight=5)
-        self.columnconfigure((1, 2), weight=1)
+        self.columnconfigure((0, 1, 2), weight=1)
         self.columnconfigure(3, weight=12)
-        self.columnconfigure(4, weight=2)
 
         self.rowconfigure(1, weight=1)
 
-        self.file_id_input = ctk.CTkEntry(self, placeholder_text='File name')
-        self.file_id_input.grid(row=0, column=0, pady=5,sticky='we')
+        self.local_btn = ctk.CTkButton(self, text='local file', command=self.load_local_command)
+        self.local_btn.grid(row=0, column=0, padx=3, pady=5)
 
-        self.drive_local_option = ctk.CTkOptionMenu(self, width=80, values=['drive', 'local'])
-        self.drive_local_option.grid(row=0, column=1, padx=2, pady=5,sticky='w')
-        
-        self.input_confirm_btn = ctk.CTkButton(self, text='Enter', command=self.input_confirm_command)
-        self.input_confirm_btn.grid(row=0, column=2, pady=5,sticky='w')
+        self.drive_btn = ctk.CTkButton(self, text='drive file', command=self.load_drive_command)
+        self.drive_btn.grid(row=0, column=1, padx=3, pady=5)
 
         self.load_from_drive_btn = ctk.CTkButton(self, text='load from drive', command=self.load_from_drive_command)
-        self.load_from_drive_btn.grid(row=0, column=3, pady=5, sticky='e')
+        self.load_from_drive_btn.grid(row=0, column=2, pady=5, sticky='e')
 
         self.clear_btn = ctk.CTkButton(self, text='clear', command=self.clear_command)
-        self.clear_btn.grid(row=0, column=4, pady=5, sticky='e')
+        self.clear_btn.grid(row=0, column=3, pady=5, sticky='e')
 
         self.excel_file = ExcelFileFrame(self)
-        self.excel_file.grid(row=1, column=0, columnspan=5, sticky="nsew")
+        self.excel_file.grid(row=1, column=0, columnspan=4, sticky="nsew")
     
-    def input_confirm_command(self):
-        choice = self.drive_local_option.get()
-        if choice == 'local':
-            file_name = self.file_id_input.get()
-            logging.info(f'file name that was entered: {file_name}')
-            self.excel_file.reload_workbook_local(file_name, 'ИУ4-23Б')
-        elif choice == 'drive':
-            file_id = self.file_id_input.get()
-            logging.info(f'file id that was entered: {file_id}')
-            self.excel_file.reload_workbook_drive(file_id, 'ИУ4-23Б')
-        else:
-            logging.error('wrong choice')
+    # def input_confirm_command(self):
+    #     choice = self.drive_local_option.get()
+    #     if choice == 'local':
+    #         file_name = self.file_id_input.get()
+    #         logging.info(f'file name that was entered: {file_name}')
+    #         self.excel_file.reload_workbook_local(file_name, 'ИУ4-23Б')
+    #     elif choice == 'drive':
+    #         file_id = self.file_id_input.get()
+    #         logging.info(f'file id that was entered: {file_id}')
+    #         self.excel_file.reload_workbook_drive(file_id, 'ИУ4-23Б')
+    #     else:
+    #         logging.error('wrong choice')
         
     def clear_command(self):
         self.excel_file.delete_workbook()
 
+    def load_local_command(self):
+        file_name = filedialog.askopenfilename()
+        if file_name:
+            self.excel_file.reload_workbook_local(file_name, 'ИУ4-23Б')
+
+    def load_drive_command(self):
+        dialog = ctk.CTkInputDialog(text='Input id:', title='file from drive')
+        input = dialog.get_input()
+        if input:
+            self.excel_file.reload_workbook_drive(input, 'ИУ4-23Б')
+                
     def load_from_drive_command(self):
         dialog = ctk.CTkInputDialog(text='Input id:', title='file from drive')
         input = dialog.get_input()
         if input:
-            filename = filedialog.asksaveasfilename()
-            excel.load_from_drive(input, filename)
+            file_name = filedialog.asksaveasfilename()
+            if file_name:
+                excel.load_from_drive(input, file_name)
 
 
 if __name__ == '__main__':
