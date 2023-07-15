850ae4ad0fea007c91894707f12bcf0ab3f03193
Bopster411 <serg.dr2004@gmail.com>
2023-07-15 19:39:16 +0300

    Added currency cmd argument

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 from project.main_app import MainApp
+from project.Webscraping.gui import get_new_exchange_rate
+import argparse, sys
 
 if __name__ == '__main__':
-    app = MainApp()
-    app.mainloop() 
+    if len(sys.argv) > 1:
+        args_parser = argparse.ArgumentParser()
+        args_parser.add_argument('-c', '--currency', help='USD -> RUB')
+        arguments = args_parser.parse_args()
+        for current_argument, current_value in vars(arguments).items():
+            if current_argument == 'currency':
+                print(f'{int(current_value) * get_new_exchange_rate(cache=False)} RUB')
+            elif current_argument in ('-p', 'phone'):
+                print('phone')
+            else:
+                print('error')
+    else:
+        app = MainApp()
+        app.mainloop() 
