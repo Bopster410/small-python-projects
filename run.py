640c839c96a1087fae9230b783e187ada10eb323
Bopster411 <serg.dr2004@gmail.com>
2023-07-16 21:07:21 +0300

    Added cmd argument for phone numbers extraction

--- run.py
+++ run.py
@@ -1,19 +1,20 @@
 from project.main_app import MainApp
 from project.Webscraping.gui import get_new_exchange_rate
+from project.Regex.phnumbers import extract_phone_number
 import argparse, sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         args_parser = argparse.ArgumentParser()
         args_parser.add_argument('-c', '--currency', help='USD -> RUB')
+        args_parser.add_argument('-p', '--phone', help='extract phone number from the string')
         arguments = args_parser.parse_args()
-        for current_argument, current_value in vars(arguments).items():
-            if current_argument == 'currency':
-                print(f'{int(current_value) * get_new_exchange_rate(cache=False)} RUB')
-            elif current_argument in ('-p', 'phone'):
-                print('phone')
-            else:
-                print('error')
+        if arguments.currency:
+            print(f'{int(arguments.currency) * get_new_exchange_rate(cache=False)} RUB')
+        elif arguments.phone:
+            print(extract_phone_number(arguments.phone))
+        else:
+            print('error')
     else:
         app = MainApp()
         app.mainloop() 
