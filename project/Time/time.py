8ebcab3401107e05c3842f1a4654f216390225bf
Bopster411 <serg.dr2004@gmail.com>
2023-07-19 19:52:27 +0300

    Got rid of taks dir in TasksManager

--- project/Time/time.py
+++ project/Time/time.py
@@ -5,46 +5,46 @@ class TaskWidget(ctk.CTkFrame):
         super().__init__(master=master, **kwargs)
         self.task_name = name
         self.time = time
-        self.text = ctk.StringVar(value=f'{self.task_name}: {self.time}')
+        self.current_time = time
+        self.text = ctk.StringVar(value=f'{self.task_name}: {self.current_time}')
         self.label = ctk.CTkLabel(self, textvariable=self.text, font=('Arial', 20))
         self.label.grid(row=0, column=0, sticky='e')
     
+    def reset_time(self):
+        self.current_time = self.time
+        self.update_label()
+
     def decrease(self):
-        if self.time != 0:
-            self.time -= 1
+        if self.current_time != 0:
+            self.current_time -= 1
             self.update_label()
 
     def update_label(self):
-        self.text.set(f'{self.task_name}: {self.time}')
+        self.text.set(f'{self.task_name}: {self.current_time}')
 
 
 class TasksManager(ctk.CTkScrollableFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, fg_color='transparent', **kwargs)
         logging.info('TasksManager object has been created')
-        self.tasks = {}
         self.tasks_widgets = []
         self.start_btn = ctk.CTkButton(self, text='start', command=self.execute_tasks)
         self.start_btn.grid(row=0, column=0)
     
     def reload_tasks_time(self):
         for task in self.tasks_widgets:
-            task.time = self.tasks[task.task_name]
+            task.reset_time()
     
     def reload_tasks_grid(self):
         for task_widget in self.tasks_widgets:
             task_widget.grid_forget()
 
-        len = 0
-        for task_name, time in self.tasks.items():
-            task_widget = TaskWidget(task_name, time, self)
-            task_widget.grid(row=len+1, column=0, sticky='ew', pady=10, padx=20)
-            self.tasks_widgets.append(task_widget)
-            len += 1
+        for row_ind, task_widget in enumerate(self.tasks_widgets):
+            task_widget.grid(row=row_ind+1, column=0, sticky='ew', pady=10, padx=20)
 
     def add_task(self, name, length):
         if len(name) > 0:
-            self.tasks[name] = length
+            self.tasks_widgets.append(TaskWidget(name, length, self))
     
     def execute_tasks(self):
         self.reload_tasks_time()
@@ -52,14 +52,16 @@ class TasksManager(ctk.CTkScrollableFrame):
         thread.start()
 
     def __execute_tasks(self):
+        self.start_btn.configure(state='disabled')
         logging.debug('Started executing tasks...')
         for task in self.tasks_widgets:
             logging.debug(f'executing next task {task.task_name}')
-            while task.time != 0:
+            while task.current_time != 0:
                 time.sleep(1)
                 task.decrease()
                 logging.debug(f'{task.task_name}: {task.time}')
         logging.debug('end executing tasks')
+        self.start_btn.configure(state='enabled')
             
 
 
