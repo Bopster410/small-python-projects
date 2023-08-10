3f405a3965284598d9a81e7efc19bc8a4b87c3c9
Bopster411 <serg.dr2004@gmail.com>
2023-08-10 19:38:37 +0300

    Moved menu buttons to the separate frame

--- project/main_app.py
+++ project/main_app.py
@@ -9,25 +9,31 @@ class MainApp(ctk.CTk):
         super().__init__()
         self.geometry('1024x800')
         self.title('epic')
-        self.rowconfigure(1, weight=1)
-        self.columnconfigure((0, 1, 2, 3), weight=1)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+
+        self.apps_frame = ctk.CTkFrame(self, width=200)
+        self.apps_frame.columnconfigure(0, weight=1)
+        self.apps_frame.grid(row=0, column=0, sticky='nsew')
         
-        self.money_app_btn = ctk.CTkButton(self, text='Converter', command=self.__to_money_app)
-        self.money_app_btn.grid(row=0, column=0, sticky='w')
+        self.money_app_btn = ctk.CTkButton(self.apps_frame, text='Converter', command=self.__to_money_app)
+        self.money_app_btn.grid(row=0, column=0, sticky='we', padx=5, pady=2)
 
-        self.files_app_btn = ctk.CTkButton(self, text='Files', command=self.__to_files_app)
-        self.files_app_btn.grid(row=0, column=1, sticky='we')
+        self.files_app_btn = ctk.CTkButton(self.apps_frame, text='Files', command=self.__to_files_app)
+        self.files_app_btn.grid(row=1, column=0, sticky='we', padx=5, pady=(0,2))
 
-        self.excel_app_btn = ctk.CTkButton(self, text='Excel', command=self.__to_excel_app)
-        self.excel_app_btn.grid(row=0, column=2, sticky='e')
+        self.excel_app_btn = ctk.CTkButton(self.apps_frame, text='Excel', command=self.__to_excel_app)
+        self.excel_app_btn.grid(row=2, column=0, sticky='we', padx=5, pady=(0,2))
 
-        self.task_manager_btn = ctk.CTkButton(self, text='Tasks', command=self.__to_time_app)
-        self.task_manager_btn.grid(row=0, column=3, sticky='e')
+        self.task_manager_btn = ctk.CTkButton(self.apps_frame, text='Tasks', command=self.__to_time_app)
+        self.task_manager_btn.grid(row=3, column=0, sticky='we', padx=5)
 
         self.app_frame = ctk.CTkFrame(self)
         self.app_frame.columnconfigure(0, weight=1)
         self.app_frame.rowconfigure(0, weight=1)
-        self.app_frame.grid(row=1, column=0, columnspan=4, sticky='nsew')
+        self.app_frame.grid(row=0, column=1, sticky='nsew')
+
+        self.apps_frame.grid_propagate(0)
 
         self.app = None
 
@@ -41,6 +47,7 @@ class MainApp(ctk.CTk):
         self.task_manager_btn.configure(state='normal')
         self.app = MoneyApp(master=self.app_frame)
         self.app.grid(row=0, column=0, sticky='nswe')
+        # self.apps_frame.grid_propagate(0)
     
     def __to_files_app(self):
         if self.app != None:
@@ -51,6 +58,7 @@ class MainApp(ctk.CTk):
         self.excel_app_btn.configure(state='normal')
         self.app = FilesApp(master=self.app_frame)
         self.app.grid(row=0, column=0, sticky='nswe')
+        # self.apps_frame.grid_propagate(0)
 
     def __to_excel_app(self):
         if self.app != None:
@@ -62,6 +70,7 @@ class MainApp(ctk.CTk):
         self.task_manager_btn.configure(state='normal')
         self.app = ExcelApp(master=self.app_frame)
         self.app.grid(row=0, column=0, sticky='nswe')
+        # self.apps_frame.grid_propagate(0)
     
     def __to_time_app(self):
         if self.app != None:
@@ -73,3 +82,4 @@ class MainApp(ctk.CTk):
         self.task_manager_btn.configure(state='disabled')
         self.app = TasksManager(master=self.app_frame)
         self.app.grid(row=0, column=0, sticky='nswe')
+        # self.apps_frame.grid_propagate(0)
