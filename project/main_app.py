197df2bcb58fdc4eb5a6a9f3aef217f08946ea28
Bopster411 <serg.dr2004@gmail.com>
2023-08-11 10:41:17 +0300

    Added command to open left panel

--- project/main_app.py
+++ project/main_app.py
@@ -10,32 +10,39 @@ class MainApp(ctk.CTk):
         self.geometry('1024x800')
         self.title('epic')
         self.rowconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
 
-        self.apps_frame = ctk.CTkFrame(self, width=200)
-        self.apps_frame.columnconfigure(0, weight=1)
-        self.apps_frame.grid(row=0, column=0, sticky='nsew')
+        self.open_left_panel_btn = ctk.CTkButton(self, text='=', width=40, height=40, command=self.__open_left_panel)
+        self.open_left_panel_btn.grid(row=0, column=0, sticky='nw', padx=2, pady=2)
+
+        self.left_panel_frame = ctk.CTkFrame(self, width=200)
+        self.left_panel_frame.columnconfigure(0, weight=1)
+        self.left_panel_frame.grid(row=0, column=1, sticky='nsew')
         
-        self.money_app_btn = ctk.CTkButton(self.apps_frame, text='Converter', command=self.__to_money_app)
+        self.money_app_btn = ctk.CTkButton(self.left_panel_frame, text='Converter', command=self.__to_money_app)
         self.money_app_btn.grid(row=0, column=0, sticky='we', padx=5, pady=2)
 
-        self.files_app_btn = ctk.CTkButton(self.apps_frame, text='Files', command=self.__to_files_app)
+        self.files_app_btn = ctk.CTkButton(self.left_panel_frame, text='Files', command=self.__to_files_app)
         self.files_app_btn.grid(row=1, column=0, sticky='we', padx=5, pady=(0,2))
 
-        self.excel_app_btn = ctk.CTkButton(self.apps_frame, text='Excel', command=self.__to_excel_app)
+        self.excel_app_btn = ctk.CTkButton(self.left_panel_frame, text='Excel', command=self.__to_excel_app)
         self.excel_app_btn.grid(row=2, column=0, sticky='we', padx=5, pady=(0,2))
 
-        self.task_manager_btn = ctk.CTkButton(self.apps_frame, text='Tasks', command=self.__to_time_app)
+        self.task_manager_btn = ctk.CTkButton(self.left_panel_frame, text='Tasks', command=self.__to_time_app)
         self.task_manager_btn.grid(row=3, column=0, sticky='we', padx=5)
 
-        self.app_frame = ctk.CTkFrame(self)
+
+        self.app_frame = ctk.CTkFrame(self, fg_color='transparent')
         self.app_frame.columnconfigure(0, weight=1)
         self.app_frame.rowconfigure(0, weight=1)
-        self.app_frame.grid(row=0, column=1, sticky='nsew')
+        self.app_frame.grid(row=0, column=2, sticky='nsew')
 
-        self.apps_frame.grid_propagate(0)
+        self.left_panel_frame.grid_propagate(0)
 
         self.app = None
+    
+    def __open_left_panel(self):
+        pass
 
     def __to_money_app(self):
         if self.app != None:
@@ -47,7 +54,7 @@ class MainApp(ctk.CTk):
         self.task_manager_btn.configure(state='normal')
         self.app = MoneyApp(master=self.app_frame)
         self.app.grid(row=0, column=0, sticky='nswe')
-        # self.apps_frame.grid_propagate(0)
+        # self.left_panel_frame.grid_propagate(0)
     
     def __to_files_app(self):
         if self.app != None:
@@ -58,7 +65,7 @@ class MainApp(ctk.CTk):
         self.excel_app_btn.configure(state='normal')
         self.app = FilesApp(master=self.app_frame)
         self.app.grid(row=0, column=0, sticky='nswe')
-        # self.apps_frame.grid_propagate(0)
+        # self.left_panel_frame.grid_propagate(0)
 
     def __to_excel_app(self):
         if self.app != None:
@@ -70,7 +77,7 @@ class MainApp(ctk.CTk):
         self.task_manager_btn.configure(state='normal')
         self.app = ExcelApp(master=self.app_frame)
         self.app.grid(row=0, column=0, sticky='nswe')
-        # self.apps_frame.grid_propagate(0)
+        # self.left_panel_frame.grid_propagate(0)
     
     def __to_time_app(self):
         if self.app != None:
@@ -82,4 +89,4 @@ class MainApp(ctk.CTk):
         self.task_manager_btn.configure(state='disabled')
         self.app = TasksManager(master=self.app_frame)
         self.app.grid(row=0, column=0, sticky='nswe')
-        # self.apps_frame.grid_propagate(0)
+        # self.left_panel_frame.grid_propagate(0)
